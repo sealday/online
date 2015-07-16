2fe1c56b07eaefc68bfec9fa5f5d280a2e4310a4
sealday <sealday@gmail.com>
2015-07-16 10:41:08 +0800

    add server user login register and auth

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
+var crypto = require('crypto');
 var url = global.MONGO_URL;
 
 // 初始化数据库
@@ -22,7 +23,35 @@ MongoClient.connect(url, function(err, db) {
 
 // 登录
 router.post('/login', function (req, res){
-
+  MongoClient.connect(url, function(err, db) {
+    var users = db.collection('users');
+    var sha1sum = crypto.createHash('sha1');
+    sha1sum.update(req.body.username + req.body.password + new Date().getTime());
+    var accessToken = sha1sum.digest('hex');
+    users.updateOne({
+      username: req.body.username,
+      password: req.body.password
+    }, {
+      $set: {
+        access_token: accessToken
+      }
+    }, function (err, result) {
+      if (err) {
+        next(err);
+      } else {
+        if (result.result.nModified == 0) {
+          res.status(401).json({
+            message: "either username or password is wrong"
+          });
+        } else {
+          res.status(200).json({
+            message: "login success",
+            access_token: accessToken
+          });
+        }
+      }
+    });
+  });
 });
 
 // 注册
@@ -40,7 +69,7 @@ router.post('/register', function (req, res, next) {
           });
         }
       } else {
-        res.json({
+        res.status(201).json({
           message: 'register success'
         });
       }
@@ -48,20 +77,28 @@ router.post('/register', function (req, res, next) {
   });
 });
 
+// 认证中间件
 router.use(function(req, res, next) {
-
-});
-
-router.use(function(req, res, next) {
-  if (req.headers['access-token']) {
-
+  var accessToken = '';
+  if (accessToken = req.headers['access-token']) {
+    MongoClient.connect(url, function(err, db) {
+      var users = db.collection('users');
+      users.findOne({access_token: accessToken}, function(err, user) {
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          res.status(403).end();
+        }
+      });
+    });
   } else {
-
+    res.status(403).end();
   }
-  next();
 });
 
-router.get('/', function(req, res) {
+router.get('/items', function(req, res) {
+  console.log(req.user);
   res.json(['hello']);
 });
 
--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ var MongoClient = mongodb.MongoClient;
 var url = MONGO_URL;
 var app;
 
-describe('POST /api/register', function() {
+describe('user api test', function() {
 
   before(function(done) {
     // 删除原来的数据库
@@ -23,37 +23,122 @@ describe('POST /api/register', function() {
   });
 
 
-  it('can register?', function(done) {
-    request(app)
-      .post('/api/register')
-      .send({username: 'seal', password: '..xiao'})
-      .expect({
-        message: 'register success'
-      })
-      .end(function(err, res) {
-        if (err) return done(err);
-        done();
-      });
+  describe('POST /api/register', function() {
+
+    it('can register?', function(done) {
+      request(app)
+        .post('/api/register')
+        .send({username: 'seal', password: '..xiao'})
+        .expect(201)
+        .expect({
+          message: 'register success'
+        })
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
+    it('can register repeat?', function(done) {
+      request(app)
+        .post('/api/register')
+        .send({username: 'sealday', password: '..xiao'})
+        .expect({
+          message: 'register success'
+        })
+        .end(function(err, res) {
+          if (err) return done(err);
+          request(app)
+            .post('/api/register')
+            .send({username: 'sealday', password: '..xiao'})
+            .expect(409)
+            .expect({
+              message: 'username has been taken'
+            })
+            .end(done);
+        });
+    });
   });
 
-  it('can register repeat?', function(done) {
-    request(app)
-      .post('/api/register')
-      .send({username: 'sealday', password: '..xiao'})
-      .expect({
-        message: 'register success'
-      })
-      .end(function(err, res) {
-        if (err) return done(err);
-        request(app)
-          .post('/api/register')
-          .send({username: 'sealday', password: '..xiao'})
-          .expect(409)
-          .expect({
-            message: 'username has been taken'
-          })
-          .end(done);
-      });
+
+  describe('POSt /api/login', function() {
+
+    before(function(done) {
+      request(app)
+        .post('/api/register')
+        .send({username: 'linda', password: '..xiao'})
+        .expect(201)
+        .end(done);
+    });
+
+    it('login with wrong password', function(done) {
+      request(app)
+        .post('/api/login')
+        .send({username: 'linda', password: '..xiao1'})
+        .expect(401)
+        .end(done);
+    });
+
+    it('login with right password', function(done) {
+      request(app)
+        .post('/api/login')
+        .send({username: 'linda', password: '..xiao'})
+        .expect(200)
+        .end(function(err, res) {
+          if (err) done(err);
+          if (res.body.access_token !== undefined) {
+            done();
+          } else {
+            done('access_token undefined');
+          }
+        });
+    });
   });
+
+  describe('test auth', function() {
+
+    var accessToken = '';
+
+    before(function(done) {
+      request(app)
+        .post('/api/register')
+        .send({username: 'lin', password: '..xiao'})
+        .expect(201)
+        .end(function() {
+          request(app)
+            .post('/api/login')
+            .send({username: 'lin', password: '..xiao'})
+            .expect(200)
+            .end(function(err, res) {
+              accessToken = res.body.access_token;
+              done();
+            });
+        });
+    });
+
+    it('get items with right token', function(done) {
+      request(app)
+        .get('/api/items')
+        .set('access-token', accessToken)
+        .expect(200)
+        .end(done);
+    });
+
+    it('get items with wrong token', function(done) {
+      request(app)
+        .get('/api/items')
+        .set('access-token', 'wrong token')
+        .expect(403)
+        .end(done);
+    });
+
+    it('get items without token', function(done) {
+      request(app)
+        .get('/api/items')
+        .expect(403)
+        .end(done);
+    });
+
+  });
+
 });
-
